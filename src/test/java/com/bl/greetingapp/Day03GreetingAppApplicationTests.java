f14f5bee06a97d3cbe263f690d1dcacc56e64ac9
sagarchavan07 <chavansagar96044@gmail.com>
2022-09-01 18:09:16 +0530

    initial commit

---
+++ src/test/java/com/bl/greetingapp/Day03GreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.greetingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day03GreetingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
